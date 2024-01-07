531c0eb547ef9964850b2da8dcf8058502c09045
Vashu Gaud <vashusharma21@gmail.com>
2024-01-07 19:04:28 +0000

    My first commit from Singapore

--- mumbai.sh
+++ mumbai.sh
@@ -2,8 +2,14 @@
 
 read -p 'Enter your Name:' a
 read -p 'Enter Your Birth Year:' b
+read -p 'Enter your Birth City:' c
 
        age=`echo "2024-$b"|bc`
-          echo "$a your age is $age"
+       if [ $c -eq 'Bulandshahr' ]
+	 then
+       echo "$a your age is $age, You belongs to Super district $c "
+         else
+       echo "$a your age is $age, You belongs to district $c"
+fi 
 
 
